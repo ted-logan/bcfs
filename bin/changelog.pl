3cf6b70d65ea30102a5d665a664899dbc4883232
Ted Logan <ted.logan@gmail.com>
2002-06-03 01:48:19 +0000

    Added support for beginning and ending time.

--- bin/changelog.pl
+++ bin/changelog.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 #
-# $Id: changelog.pl,v 1.1 2002-05-19 22:51:41 jaeger Exp $
+# $Id: changelog.pl,v 1.2 2002-06-03 01:48:19 jaeger Exp $
 #
 
 # 28 May 2000
@@ -14,31 +14,25 @@ use Getopt::Long;
 use lib '/home/jaeger/programming/webpage/lib';
 use Jaeger::Changelog;
 
-my $seconds; # seconds from the epoch
-my $timestamp; # timestamp entry
-my $date; # temporary varible
+my ($time_begin, $time_end); # temporary varible
 my $title;
-GetOptions('date=s', \$date, "title=s", \$title);
-if($date) {
-	$seconds = `date --date="$date" +"%s"`;
-} else {
-	$seconds = time;
-}
+GetOptions('date=s',\$time_begin, 'time_end=s',\$time_end, "title=s",\$title);
+
+$time_begin = timestamp($time_begin);
 
 my $tempfile = shift;
 
-$timestamp = scalar localtime $seconds;
-
 my $changelog = new Jaeger::Changelog();
-$changelog->time_begin($timestamp);
+$changelog->time_begin($time_begin);
 
 unless($title) {
 	print "New Changelog Entry\n";
-	print "Date: $timestamp\n";
+	print "Date: $time_begin\n";
 	print "Title: ";
 	$title = <>;
 	chomp $title;
-}
+
+
 $changelog->title($title);
 
 unless($tempfile) {
@@ -47,6 +41,8 @@ unless($tempfile) {
 
 system qq(vi $tempfile "+set textwidth=72");
 if(-s $tempfile) {
+	$time_end = timestamp($time_end);
+
 	# spellcheck it
 	system "ispell $tempfile";
 
@@ -62,3 +58,11 @@ if(-s $tempfile) {
 }
 close TF;
 unlink $tempfile;
+
+sub timestamp {
+	my $time = shift;
+
+	return scalar localtime($time ?
+			`date --date="$time_begin" +"%s"` :
+			time);
+}
