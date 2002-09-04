a6c7d4287fe170cb77e39aa14851d0d2fb4d2e86
Ted Logan <ted.logan@gmail.com>
2002-09-04 15:59:25 +0000

    Added support for specifying a changelog to edit by id.

--- bin/changelog.pl
+++ bin/changelog.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 #
-# $Id: changelog.pl,v 1.3 2002-08-26 06:22:36 jaeger Exp $
+# $Id: changelog.pl,v 1.4 2002-09-04 15:59:25 jaeger Exp $
 #
 
 # 28 May 2000
@@ -16,24 +16,47 @@ use Jaeger::Changelog;
 
 my ($time_begin, $time_end); # temporary varible
 my $title;
-GetOptions('date=s',\$time_begin, 'time_end=s',\$time_end, "title=s",\$title);
-
-$time_begin = timestamp($time_begin);
+my $id;
+GetOptions(
+	'date=s' => \$time_begin,
+	'time_end=s' => \$time_end,
+	"title=s" => \$title,
+	"id=i" => \$id
+);
 
 my $tempfile = shift;
 
-my $changelog = new Jaeger::Changelog();
-$changelog->time_begin($time_begin);
+my $changelog;
+if($id) {
+	$changelog = new Jaeger::Changelog($id);
+	unless($changelog) {
+		die "Changelog with id = $id doesn't exist\n";
+	}
+	print "Selecting changelog id = $id (", $changelog->title(), ")\n";
 
-unless($title) {
-	print "New Changelog Entry\n";
-	print "Date: $time_begin\n";
-	print "Title: ";
-	$title = <>;
-	chomp $title;
+} else {
+	$changelog = new Jaeger::Changelog();
+
+	$time_begin = timestamp($time_begin);
+	$time_end = timestamp($time_end);
+
+	if($time_begin) {
+		$changelog->time_begin($time_begin);
+	}
+	if($time_end) {
+		$changelog->time_end($time_end);
+	}
+
+	unless($title) {
+		print "New Changelog Entry\n";
+		print "Date: $time_begin\n";
+		print "Title: ";
+		$title = <>;
+		chomp $title;
+	}
+
+	$changelog->title($title);
 }
-
-$changelog->title($title);
 
 $changelog->edit($tempfile);
 
