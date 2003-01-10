2394b1df0c36279afc604fb8554fca48ffb65bc8
Ted Logan <ted.logan@gmail.com>
2003-01-10 06:55:37 +0000

    Added relevant tables and views for the Epic Photo Update.

--- lib/Jaeger/db.sql
+++ lib/Jaeger/db.sql
@@ -26,3 +26,35 @@ create table content (
 	timestamp datetime default now() not null,
 	value text
 );
+
+create table timezone (
+	id		serial primary key,
+	name		text not null unique,
+	ofst		int4 not null
+);
+
+create table location (
+	id		serial primary key,
+	state		text,
+	city		text,
+	name		text not null,
+	unique(state, city, name)
+);
+
+create table photo (
+	id		serial primary key,
+	round		text not null,
+	number		text not null,
+	unique(round, number),
+	date		int4 not null,
+	timezone_id	int4 references timezone,
+	location_id	int4 references location,
+	hidden		boolean not null default false,
+	description	text
+);
+
+create view photo_date as select
+	photo.id,
+	((date + ofst * 3600) / 86400) * 86400 as "date"
+	from photo, timezone
+	where not photo.hidden and timezone_id = timezone.id and photo.date > 0;
