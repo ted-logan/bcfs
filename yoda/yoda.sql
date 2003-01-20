4621023afec65c8d817c8d49d17007263afcece5
Ted Logan <ted.logan@gmail.com>
2003-01-20 20:09:11 +0000

    Added boolean valid column to specify when gas mileage should be calculated for a row.

--- yoda/yoda.sql
+++ yoda/yoda.sql
@@ -1,3 +1,7 @@
+--
+-- $Id: yoda.sql,v 1.2 2003-01-20 20:09:11 jaeger Exp $
+--
+
 create table yoda (
 	date date not null,
 	station text not null,
@@ -6,5 +10,6 @@ create table yoda (
 	mileage int4 not null,
 	ppg float not null,
 	gal float not null,
-	total float not null
+	total float not null,
+	valid boolean not null default(true)
 );
