5e22d5b4f0a887fbd2c696e1f2a4e37f1cf32519
Ted Logan <ted.logan@gmail.com>
2003-01-20 19:39:34 +0000

    Allows editing and creation of static content.

---
+++ bin/content.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl
+
+#
+# $Id: content.pl,v 1.1 2003-01-20 19:39:34 jaeger Exp $
+#
+
+# content.pl: Allows editing of static content
+#
+# 16 January 2003
+# Ted Logan (jaeger@festing.org)
+
+use strict;
+
+use lib '/home/jaeger/programming/webpage/lib';
+
+use Jaeger::Content;
+
+# figure out what content we're using
+my $title = shift;
+
+unless($title) {
+	die "Usage: $0 TITLE [PARENT]\n"; 
+}
+
+my $content = Jaeger::Content->Select(label => $title);
+
+unless($content) {
+	my $parent = shift;
+
+	$content = new Jaeger::Content;
+	$content->{label} = $title;
+	$content->{parent} = $parent;
+}
+
+print "Title: $title", ($content->{timestamp} ? '' : ' (new)'), "\n";
+if($content->{parent}) {
+	print "Parent: $content->{parent}\n";
+} else {
+	print "Parent: (null)\n";
+}
+
+{
+	my @sibblings = map {$_->{label}} $content->sibblings();
+	print "Sibblings: ", (@sibblings ? "@sibblings" : '(none)'), "\n";
+}
+
+{
+	my @children = map {$_->{label}} $content->children();
+	print "Children: ", (@children ? "@children" : '(none)'), "\n";
+}
+
+{
+	my @commands = (
+		e => 'Edit',
+		i => 'Use ispell(1)',
+		y => 'Insert',
+		q => 'Abandon changes',
+	);
+
+	my %commands = @commands;
+
+	sub menu {
+		my $choice;
+
+		do {
+			# show the menu
+			print "\n";
+			for(my $i = 0; $i < @commands; $i += 2) {
+				print "($commands[$i]) $commands[$i + 1]\n";
+			}
+
+			# get input
+			print "Master? ";
+			$choice = lc <STDIN>;
+			chomp $choice;
+
+		} until(exists $commands{$choice});
+
+		return $choice;
+	}
+}
+
+my $dirty = 0;
+
+while(1) {
+	my $choice = menu();
+
+	if($choice eq 'e') {
+		$dirty ||= $content->pipe(qq(vi "+set textwidth=72"));
+
+	} elsif($choice eq 'i') {
+		$dirty ||= $content->pipe('ispell');
+
+	} elsif($choice eq 'y') {
+		if($dirty) {
+			$content->{timestamp} = scalar localtime time;
+			$content->update();
+		}
+		exit;
+
+	} elsif($choice eq 'q') {
+		# abandon any changes
+		exit;
+	}
+}
