ea4c6fdb54f7c88c962f50989311acf9317c8a24
Ted Logan <ted.logan@gmail.com>
2003-01-20 19:37:49 +0000

    Creates or updates thumbnails for a round of photos or the entire set.

---
+++ bin/thumbnail.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+#
+# $Id: thumbnail.pl,v 1.1 2003-01-20 19:37:49 jaeger Exp $
+#
+
+# create thumbnail images for the given rounds if they don't already exist
+
+use strict;
+
+use lib '/home/jaeger/programming/webpage/lib';
+
+use Jaeger::Photo;
+
+use Image::Magick;
+
+my @rounds;
+
+if(@ARGV) {
+	@rounds = @ARGV;
+} else {
+	opendir DIR, $Jaeger::Photo::Dir
+		or die "Can't open photo dir: $!\n";
+	@rounds = sort grep {!/^\./ && -d "$Jaeger::Photo::Dir/$_"} readdir DIR;
+	closedir DIR;
+}
+
+foreach my $round (@rounds) {
+	unless(-d "$Jaeger::Photo::Dir/$round/thumbnail") {
+		mkdir "$Jaeger::Photo::Dir/$round/thumbnail", 0755;
+	}
+
+	my @photos = Jaeger::Photo->Select(round => $round);
+
+	foreach my $photo (@photos) {
+		print "$round/$photo->{number}: ", $photo->file(), "\n";
+
+		next unless $photo->file();
+
+		# figure out if we need to create or update the thumbnail
+		if(!-f $photo->thumbnail() ||
+				((stat $photo->thumbnail())[9] <
+				(stat $photo->file())[9])) {
+			print "\tUpdating thumbnail\n";
+
+			my $img = new Image::Magick;
+			$img->Read($photo->file());
+
+			# we want to scale the image to fit within 128x96
+			my ($x, $y) = $img->Get('columns', 'rows');
+			if($x == 0 || $y == 0) {
+				print "\tInvalid file!\n";
+				next;
+			}
+
+			my $aspect = $x / $y;
+			my ($newx, $newy);
+			if($aspect > 4 / 3) {
+				$newx = 128;
+				$newy = 128 / $aspect;
+			} else {
+				$newy = 96;
+				$newx = 96 * $aspect;
+			}
+			print "\t($x, $y) -> ($newx, $newy)\n";
+			$img->Resize(width => $newx, height => $newy);
+			$img->Write($photo->thumbnail());
+		}
+	}
+
+}
