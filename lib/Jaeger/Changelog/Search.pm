181199b1064b2a8945efb0aac5c5794d0f24c32e
Ted Logan <ted.logan@gmail.com>
2003-01-26 12:48:39 +0000

    Module to work under Jaeger::Search::Searchable framework for searching changelogs.

---
+++ lib/Jaeger/Changelog/Search.pm
@@ -0,0 +1,48 @@
+package Jaeger::Changelog::Search;
+
+#
+# $Id: Search.pm,v 1.1 2003-01-26 12:48:39 jaeger Exp $
+#
+
+# package to allow searching of changelogs
+
+# created  26 January 2003
+
+use strict;
+
+use Jaeger::Search::Searchable;
+use Jaeger::Changelog;
+
+@Jaeger::Changelog::Search::ISA = qw(Jaeger::Search::Searchable);
+
+# returns a list containing the changelogs for this search
+sub _content {
+	my $self = shift;
+
+	my $search = $self->{search};
+
+	my @cl = Jaeger::Changelog->Select($search->like(qw(title content)));
+
+	# rank the changelogs
+	foreach my $changelog (@cl) {
+		$changelog->{rank} = $search->rank(
+			$changelog->{title}, $changelog->{content}
+		);
+	}
+
+	return @cl;
+}
+
+sub what {
+	return 'changelogs';
+}
+
+#
+# methods used by Jaeger::Search::Searchable to show this page
+#
+
+sub _title {
+	my $self = shift;
+
+	return $self->{title} = 'Changelog Search Results';
+}
