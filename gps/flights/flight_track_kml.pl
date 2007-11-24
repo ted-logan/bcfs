61f6bc0bb22a3a2bf0182e934126870834f6c783
Ted Logan <ted.logan@gmail.com>
2007-11-24 03:54:40 +0000

    Read all of the track files in the current directory and output a single kml file containing them all.

--- gps/flights/flight_track_kml.pl
+++ gps/flights/flight_track_kml.pl
@@ -2,11 +2,14 @@
 
 use strict;
 
-print <<XML;
+open KML, ">flights.kml"
+	or die "Can't write flights.kml: $!\n";
+
+print KML <<XML;
 <?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://earth.google.com/kml/2.1">
   <Document>
-    <name>This is a flight</name>
+    <name>Flights</name>
     <Style id="track">
       <LineStyle>
         <color>c00000ff</color>
@@ -16,8 +19,16 @@ print <<XML;
         <color>c00000ff</color>
       </PolyStyle>
     </Style>
+XML
+
+foreach my $file (sort <*.txt>) {
+	my $title = $file;
+	$title =~ s/\.txt$//;
+	$title =~ s/-([a-z]+)/ \U$1/i;
+
+	print KML <<XML;
     <Placemark>
-      <name>Flight</name>
+      <name>$title</name>
       <styleUrl>#track</styleUrl>
       <LineString>
         <extrude>0</extrude>
@@ -25,22 +36,27 @@ print <<XML;
         <coordinates>
 XML
 
-
-while(<>) {
-	chomp;
-	my @data = split;
-	my $lat = $data[1];
-	my $lon = $data[2];
-	my $alt = $data[4] * 0.4048;
-	if($lat != 0 || $lon != 0) {
-		print "          $lon,$lat,$alt\n";
+	open TRACK, $file;
+	while(<TRACK>) {
+		chomp;
+		my @data = split;
+		my $lat = $data[1];
+		my $lon = $data[2];
+		my $alt = $data[4] * 0.4048;
+		if($lat != 0 || $lon != 0) {
+			print KML "          $lon,$lat,$alt\n";
+		}
 	}
-}
+	close TRACK;
 
-print <<XML;
+	print KML <<XML;
         </coordinates>
       </LineString>
     </Placemark>
+XML
+}
+
+print KML <<XML;
   </Document>
 </kml>
 XML
