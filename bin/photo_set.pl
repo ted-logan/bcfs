868462de7cbef207b8f8599dc6f8116250eaec8c
Ted Logan <ted.logan@gmail.com>
2015-06-20 21:30:16 +0000

    Include photos with status <= 10 in the photo sets.

--- bin/photo_set.pl
+++ bin/photo_set.pl
@@ -10,6 +10,10 @@ die "\$BCFS must be set!\n" unless $ENV{BCFS};
 use lib "$ENV{BCFS}/lib";
 use Jaeger::Photo;
 use Jaeger::Photo::Set;
+use Jaeger::User;
+
+$Jaeger::User::Current = new Jaeger::User();
+$Jaeger::User::Current->{status} = 10;
 
 foreach my $set (Jaeger::Photo::Set->Select()) {
 	my $dir = $set->directory();
