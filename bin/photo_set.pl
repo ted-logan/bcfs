f082170b39a67ce91f05a90b05496776e36a7135
Ted Logan <ted.logan@gmail.com>
2022-08-19 05:39:22 +0000

    Scale photos saved to the photo set down to 1600x1200

--- bin/photo_set.pl
+++ bin/photo_set.pl
@@ -11,6 +11,7 @@ use lib "$ENV{BCFS}/lib";
 use Encode qw(decode);
 use File::Copy;
 use File::Path qw(make_path);
+use Image::Magick;
 use Jaeger::Photo;
 use Jaeger::Photo::Set;
 use Jaeger::User;
@@ -54,10 +55,29 @@ foreach my $set (Jaeger::Photo::Set->Select()) {
 		       		decode("utf-8", $photo->description()),
 				")\n";
 			if($photo->file_crop()) {
-				copy($photo->file_crop(), "$dir/$file")
-					or warn "Can't copy ",
-						$photo->{round}, "_",
-						$photo->{number}, ": $!\n";
+				# Check if the photo is larger than the
+				# 1600x1200. (This is larger than my current
+				# picture frame, but it's still within the
+				# grasp of the picture frame. As of 2022-08, my
+				# picture frame is a NIX-10H, with a screen
+				# resolution of 1280x800.)
+				my $img = new Image::Magick;
+				$img->Read($photo->file_crop());
+				my ($width, $height) = $img->Get('width', 'height');
+				if($width > 1600 || $height > 1200) {
+					# Resize the 
+					$img->Resize(geometry => "1600x1200>");
+					my $err = $img->Write("$dir/$file");
+					if("$err") {
+						warn "$err";
+					}
+				} else {
+					# Copy the existing photo directly
+					copy($photo->file_crop(), "$dir/$file")
+						or warn "Can't copy ",
+							$photo->{round}, "_",
+							$photo->{number}, ": $!\n";
+				}
 				$new++;
 			} else {
 				warn "Photo not found: $photo->{round}_$photo->{number}\n";
