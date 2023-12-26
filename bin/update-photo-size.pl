1672cb552534b47d9626bf96401d6caa5fd2eaec
Ted Logan <ted.logan@gmail.com>
2023-12-26 23:16:09 +0000

    A script to update the photo size of all photos in the database where it's missing

---
+++ bin/update-photo-size.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+# Find all photos in the database that are visible and have a null width or
+# height. Update their width and height from the physical dimensions of the
+# photo.
+
+use strict;
+
+die "\$BCFS must be set!\n" unless $ENV{BCFS};
+
+use lib "$ENV{BCFS}/lib";
+
+use Jaeger::Photo;
+use Jaeger::User;
+
+binmode STDOUT, ':utf8';
+
+$Jaeger::User::Current = new Jaeger::User();
+$Jaeger::User::Current->{status} = 30;
+
+my $where = "(width is null or height is null) and not hidden";
+
+foreach my $photo (Jaeger::Photo->Select($where)) {
+	printf "%s/%s: %s\n", $photo->round(), $photo->number(),
+       		$photo->description();
+
+	# Update the width and height of the photo stored in the database
+	my $img = new Image::Magick;
+	$img->Read($photo->file_crop());
+	my ($width, $height) = $img->Get('width', 'height');
+	$photo->{width} = $width;
+	$photo->{height} = $height;
+
+	printf "\t%dx%d\n", $width, $height;
+
+	$photo->update();
+}
